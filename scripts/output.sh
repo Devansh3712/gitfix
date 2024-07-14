9860e1f1d825011f823c3225861573479b4b1d13
Devansh Singh <devanshamity@gmail.com>
2024-07-14 14:27:25 +0530

    Remove -a flag from tee command
    Signed-off-by: Devansh Singh <devanshamity@gmail.com>

--- scripts/output.sh
+++ scripts/output.sh
@@ -2,7 +2,7 @@ function gf() {
 	command mkdir -p $HOME/.gitfix
 	log_file="$HOME/.gitfix/git.log"
 	echo "git $@" > "$log_file"
-	command git "$@" 2>&1 | tee -a "$log_file"
+	command git "$@" 2>&1 | tee "$log_file"
 
 	if [ ${PIPESTATUS[0]} -ne 0 ]; then
 		gitfix
